975bea60acc7241c318fdb94fdbd3e7f3db9baa5
jonapatig <jona.patig@gmail.com>
2022-01-22 22:13:35 +0100

    Counts amount of each letter in each spot.

---
+++ Wordle Solver.py
@@ -0,0 +1,29 @@
+try:
+    with open('C:/Users/Jona/Documents/5letterwords.txt', 'r') as f:
+        raw = f.read()
+except:
+    with open('C:/Users/jonap/Documents/5letterwords.txt', 'r') as f:
+        raw = f.read()
+
+possible = raw.splitlines()
+counts0 = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+counts1 = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+counts2 = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+counts3 = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+counts4 = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+for i in possible:
+    counts0[i[0]]=counts0[i[0]]+1
+    counts1[i[1]]=counts0[i[1]]+1
+    counts2[i[2]]=counts0[i[2]]+1
+    counts3[i[3]]=counts0[i[3]]+1
+    counts4[i[4]]=counts0[i[4]]+1
+print(counts0)
+print(counts1)
+print(counts2)
+print(counts3)
+print(counts4)
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+total = 0
+for l in alphabet:
+    total += counts0[l]
+print(total)
